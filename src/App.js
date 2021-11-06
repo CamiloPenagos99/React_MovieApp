52bd059cd472801a3052fd0b1b1a59cc305f183b
camilo penagos <penagoscamilo99@gmail.com>
2021-11-06 16:12:14 -0500

    se agrega el componente movie y manejo de props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Pelicula from './componentes/pelicula';
 
 function App() {
 
@@ -228,17 +229,14 @@ function App() {
 					<a href="movielist.html" className="list"><i className="ion-ios-list-outline active"></i></a>
 					<a  href="moviegrid.html" className="grid"><i className="ion-grid"></i></a>
 				</div>
-				<div className="movie-item-style-2">
-					<img src="images/uploads/mv1.jpg" alt="" />
-					<div className="mv-item-infor">
-						<h6><a href="moviesingle.html">oblivion <span>(2012)</span></a></h6>
-						<p className="rate"><i className="ion-android-star"></i><span>8.1</span> /10</p>
-						<p className="describe">Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity...</p>
-						<p className="run-time"> Run Time: 2h21’    .     <span>MMPA: PG-13 </span>    .     <span>Release: 1 May 2015</span></p>
-						<p>Director: <a href="#">Joss Whedon</a></p>
-						<p>Stars: <a href="#">Robert Downey Jr.,</a> <a href="#">Chris Evans,</a> <a href="#">  Chris Hemsworth</a></p>
-					</div>
-				</div>
+
+			<Pelicula name={"Oblivion 1"}/>
+			<Pelicula name={"Oblivion 2"}/>
+			<Pelicula name={"Oblivion 3"}/>
+			<Pelicula name="Oblivion 4"/>
+			<Pelicula name={2022}/>
+			
+
 				<div className="movie-item-style-2">
 					<img src="images/uploads/mv2.jpg" alt="" />
 					<div className="mv-item-infor">
